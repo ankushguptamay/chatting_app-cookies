c65f401e1cda24632328411acfff4e70bbb9c414
Ankush Gupta <ankushgupta9675@gmail.com>
2024-08-01 14:57:54 +0530

    tring to solve error

--- Middleware/verifyJWTToken.js
+++ Middleware/verifyJWTToken.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const db = require("../Model");
 const User = db.user;
+const { ErrorHandler } = require("../Utils/helper");
 const { USER_JWT_SECRET_KEY, ADMIN_JWT_SECRET_KEY } = process.env;
 
 exports.verifyUserJWT = (req, res, next) => {
@@ -44,28 +45,19 @@ exports.socketAuthenticator = async (err, socket, next) => {
     const authToken = socket.request.cookies["chat-user-token"];
 
     if (!authToken)
-      return res
-        .status(401)
-        .send({ success: false, message: "Please login to access this route" });
+      return next(new ErrorHandler("Please login to access this route", 401));
 
     const decodedData = jwt.verify(authToken, process.env.USER_JWT_SECRET_KEY);
 
     const user = await User.findOne({ where: { id: decodedData.id } });
 
     if (!user)
-      return res.status(401).send({
-        success: false,
-        message: "Please Rejister to access this route",
-      });
-
+      return next(new ErrorHandler("Please login to access this route", 401));
     socket.user = user;
 
     return next();
   } catch (error) {
     console.log(error);
-    return res.status(500).send({
-      success: false,
-      message: error,
-    });
+    return next(new ErrorHandler("Please login to access this route", 401));
   }
 };
--- Utils/helper.js
+++ Utils/helper.js
@@ -1,37 +1,51 @@
-const fs = require('fs');
+const fs = require("fs");
 
-exports.getOtherMember = (members, userId) => {
-    const newMembers = [];
-    for (let i = 0; i < members.length; i++) {
-        if (members[i].userId !== userId) {
-            newMembers.push(members[i]);
-        }
+const getOtherMember = (members, userId) => {
+  const newMembers = [];
+  for (let i = 0; i < members.length; i++) {
+    if (members[i].userId !== userId) {
+      newMembers.push(members[i]);
     }
-    return newMembers;
-}
+  }
+  return newMembers;
+};
 
-exports.getSingleChat = (chat, userId) => {
-    const otherMember = this.getOtherMember(chat.members, userId);
-    return transForm = {
-        id: chat.id,
-        chatName: chat.isGroup ? chat.chatName : otherMember[0].userName,
-        members: otherMember,
-        isGroup: chat.isGroup,
-        avatar_url: chat.isGroup ? chat.avatar_url : otherMember[0].avatar_url,
-        creator: chat.creator
-    };
-}
+const getSingleChat = (chat, userId) => {
+  const otherMember = this.getOtherMember(chat.members, userId);
+  return (transForm = {
+    id: chat.id,
+    chatName: chat.isGroup ? chat.chatName : otherMember[0].userName,
+    members: otherMember,
+    isGroup: chat.isGroup,
+    avatar_url: chat.isGroup ? chat.avatar_url : otherMember[0].avatar_url,
+    creator: chat.creator,
+  });
+};
 
-exports.deleteSingleFile = (filePath) => {
-    if (filePath) {
-        // console.log(fs.existsSync(filePath));
-        if (fs.existsSync(filePath)) {
-            fs.unlink(filePath, (err) => {
-                if (err) {
-                    throw (err);
-                }
-            })
+const deleteSingleFile = (filePath) => {
+  if (filePath) {
+    // console.log(fs.existsSync(filePath));
+    if (fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          throw err;
         }
+      });
     }
-    return;
+  }
+  return;
+};
+
+class ErrorHandler extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+  }
 }
+
+module.exports = {
+  ErrorHandler,
+  deleteSingleFile,
+  getSingleChat,
+  getOtherMember,
+};
