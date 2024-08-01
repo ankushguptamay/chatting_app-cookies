0e1ddea91b49c5e3a0fba255e274f506cb758647
Ankush Gupta <ankushgupta9675@gmail.com>
2024-08-01 11:05:38 +0530

    error solve in quth socket

--- Middleware/verifyJWTToken.js
+++ Middleware/verifyJWTToken.js
@@ -5,29 +5,35 @@ const { USER_JWT_SECRET_KEY, ADMIN_JWT_SECRET_KEY } = process.env;
 
 exports.verifyUserJWT = (req, res, next) => {
   const token = req.cookies["chat-user-token"];
+
   if (!token) return res.sendStatus(401);
+
   jwt.verify(token, USER_JWT_SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(401).send({
+        success: false,
         message: "Unauthorized!",
       });
     }
     req.user = decoded;
-    next();
+    return next();
   });
 };
 
 exports.verifyAdminJWT = (req, res, next) => {
   const token = req.cookies["chat-admin-token"];
+
   if (!token) return res.sendStatus(401);
+
   jwt.verify(token, ADMIN_JWT_SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(401).send({
+        success: false,
         message: "Unauthorized!",
       });
     }
     req.admin = decoded;
-    next();
+    return next();
   });
 };
 
@@ -35,25 +41,31 @@ exports.socketAuthenticator = async (err, socket, next) => {
   try {
     if (err) return next(err);
 
-    if (err) return next(err);
-
     const authToken = socket.request.cookies["chat-user-token"];
 
     if (!authToken)
-      return next(new ErrorHandler("Please login to access this route", 401));
+      return res
+        .status(401)
+        .send({ success: false, message: "Please login to access this route" });
 
-    const decodedData = jwt.verify(authHeader, process.env.USER_JWT_SECRET_KEY);
+    const decodedData = jwt.verify(authToken, process.env.USER_JWT_SECRET_KEY);
 
     const user = await User.findOne({ where: { id: decodedData.id } });
 
     if (!user)
-      return next(new ErrorHandler("Please login to access this route", 401));
+      return res.status(401).send({
+        success: false,
+        message: "Please Rejister to access this route",
+      });
 
     socket.user = user;
 
     return next();
   } catch (error) {
     console.log(error);
-    return next(new ErrorHandler("Please login to access this route", 401));
+    return res.status(500).send({
+      success: false,
+      message: error,
+    });
   }
 };
